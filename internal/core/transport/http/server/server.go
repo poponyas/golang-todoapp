21620651a877c639a104abd7101a9f847f93ac84
Rodion <rodionsarahov@gmail.com>
2026-08-31 15:11:08 +0300

    add swagger spec

--- internal/core/transport/http/server/server.go
+++ internal/core/transport/http/server/server.go
@@ -6,9 +6,12 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/poponyas/golang-todoapp/docs"
 	core_logger "github.com/poponyas/golang-todoapp/internal/core/logger"
 	core_http_middleware "github.com/poponyas/golang-todoapp/internal/core/transport/http/middleware"
 	"go.uber.org/zap"
+
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
 type HTTPServer struct {
@@ -37,6 +40,24 @@ func (s *HTTPServer) RegisterAPIRouters(routers ...*APIVersionRouter) {
 			http.StripPrefix(prefix, router.WithMiddleware()),
 		)
 	}
+}
+
+func (s *HTTPServer) RegisterSwagger() {
+	s.mux.Handle(
+		"/swagger/",
+		httpSwagger.Handler(
+			httpSwagger.URL("/swagger/doc.json"),
+			httpSwagger.DefaultModelsExpandDepth(-1),
+		),
+	)
+	s.mux.HandleFunc(
+		"/swagger/doc.json",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			_, _ = w.Write([]byte(docs.SwaggerInfo.ReadDoc()))
+		},
+	)
 }
 
 func (s *HTTPServer) Run(ctx context.Context) error {
